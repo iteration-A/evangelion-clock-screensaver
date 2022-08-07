c18e2797079e592edc2e5515279bc087ad149e8f
Manuel Luna <manuellunada@gmail.com>
2022-08-06 22:49:49 -0500

    how do i compile this? ;-;

--- webview/script.js
+++ webview/script.js
@@ -8,45 +8,19 @@ function setDigit(number, text) {
 	element("clock-digit-" + number).children[0].textContent = text;
 }
 
-function setFixedTimeForDebug() {
-	setDigit(1, 2);
-	setDigit(2, 3);
-	setDigit(3, 4);
-	setDigit(4, 8);
-	setDigit(5, 2);
-	setDigit(6, 3);
-}
-
 function updateClock() {
-	var date = new Date();
+	const date = new Date();
 
-	var hoursInt = date.getHours();
-	var minutesInt = date.getMinutes();
-	var secondsInt = date.getSeconds();
+	const [, time] = date.toLocaleString();
+	const [hour, minute, second] = time.split(":");
 
-	var hoursString = hoursInt + "";
-	var minutesString = minutesInt + "";
-	var secondsString = secondsInt + "";
-
-	if (hoursInt < 10) {
-		hoursString = "0" + hoursInt;
-	}
-	if (minutesString < 10) {
-		minutesString = "0" + minutesString;
-	}
-	if (secondsString < 10) {
-		secondsString = "0" + secondsString;
-	}
-
-	setDigit(1, hoursString.charAt(0));
-	setDigit(2, hoursString.charAt(1));
-	setDigit(3, minutesString.charAt(0));
-	setDigit(4, minutesString.charAt(1));
-	setDigit(5, secondsString.charAt(0));
-	setDigit(6, secondsString.charAt(1));
+	setDigit(1, hour.charAt(0));
+	setDigit(2, hour.charAt(1));
+	setDigit(3, minute.charAt(0));
+	setDigit(4, minute.charAt(1));
+	setDigit(5, second.charAt(0));
+	setDigit(6, second.charAt(1));
 }
 
 setInterval(updateClock, 1000);
 updateClock();
-
-// setFixedTimeForDebug();
